3635f0b2845e7d6ad03704a73818586eb97466e3
Nico <ubncr@student.kit.edu>
2023-12-31 18:42:57 +0100

    Integrated Google Map with marker

---
+++ main.js
@@ -0,0 +1,27 @@
+// Initialize and add the map
+let map;
+
+async function initMap() {
+  // The location of New York City
+  const position = { lat: 40.71992149783236, lng: -74.03880759381295 };
+
+  // Request the necessary libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  // const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
+
+  // The map, centered at New York City
+  map = new Map(document.getElementById("map"), {
+    zoom: 10,
+    center: position,
+    mapId: "Discover Map",
+  });
+
+  new google.maps.Marker({
+    position: position,
+    map,
+    title: "Default Location",
+  });
+}
+
+initMap();
