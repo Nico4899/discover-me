33c7b3e58149b3ed7dddd45ecc7a9a808202d8d2
Nico <ubncr@student.kit.edu>
2024-01-04 17:09:21 +0100

    Add instagram login

---
+++ src/scripts/instagram-login.js
@@ -0,0 +1,9 @@
+document.getElementById('instagram-connect-btn').addEventListener('click', function() {
+    var clientId = '1081191459738539'; // Replace with your actual Instagram App ID
+    var redirectUri = encodeURIComponent('https://nico4899.github.io/discover-me/'); 
+    var authUrl = 'https://api.instagram.com/oauth/authorize?client_id=' + clientId + 
+                    '&redirect_uri=' + redirectUri + 
+                    '&scope=user_profile,user_media&response_type=code';
+
+    window.open(authUrl, 'InstagramLogin', 'width=800,height=600');
+});
